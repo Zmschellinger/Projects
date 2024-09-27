9edc7f89f49d8380e37cf9442bd06cf4ac3d34a7
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-27 07:37:22 -0400

    Create 2negMod5.s

---
+++ CompOrg/2negMod5.s
@@ -0,0 +1,5 @@
+# Program Name : 2negMod5.asm  
+# Creator : Zachary Schellinger
+# EN.605.204.81.FA24
+# Homework for module 5
+# This program will take an integern number and write out the negative value of the number. 
