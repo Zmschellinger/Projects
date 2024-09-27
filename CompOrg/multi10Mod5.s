47a6591382ae83434c29075418ca09351024d371
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-27 07:49:40 -0400

    Create multi10Mod5.s

---
+++ CompOrg/multi10Mod5.s
@@ -0,0 +1,5 @@
+# Program Name : multi10Mod5.s 
+# Creator : Zachary Schellinger
+# EN.605.204.81.FA24
+# Homework for module 5
+# This program reads an input then conducts a left logical shift and add in order to nultiply a number by 10
