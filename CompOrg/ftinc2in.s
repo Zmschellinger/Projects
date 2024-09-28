5b37a4300875878a64391d9a7a7fe4f090d457cd
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-28 09:33:25 -0400

    Update ftinc2in.s

--- CompOrg/ftinc2in.s
+++ CompOrg/ftinc2in.s
@@ -1,1 +1,6 @@
-
+# Program Name : cel2FarMod5.asm  
+# Creator : Zachary Schellinger
+# EN.605.204.81.FA24
+# Homework for module 5
+# This program converts ft and inch, to inch.
+# Takes the 2 inputs, one for ft and one for inches, then outputs the sum in inches. 
