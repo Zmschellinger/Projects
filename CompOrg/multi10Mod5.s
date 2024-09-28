b6f09d6f8200fc28a806d6abdea2b6f2e23151a5
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-28 08:53:36 -0400

    Update multi10Mod5.s

--- CompOrg/multi10Mod5.s
+++ CompOrg/multi10Mod5.s
@@ -2,4 +2,49 @@
 # Creator : Zachary Schellinger
 # EN.605.204.81.FA24
 # Homework for module 5
-# This program reads an input then conducts a left logical shift and add in order to nultiply a number by 10
+# This program reads an input then conducts a left logical shift and add in order to multiply a number by 10
+.text
+.global main
+
+main: 
+  #Save return to os on stack (from textbook)
+  SUB sp, sp, #4
+  STR lr, [sp, #0]
+
+  #Ask user for their age
+  LDR r0, =question
+  BL printf
+
+  # scanf
+  LDR r0, =input
+  LDR r1, =num
+  BL scanf
+  
+  #mulitply by 10
+  
+  
+  # print message
+  LDR r0, =format
+  LDR r1, =num
+  LDR r1, [r1, #0]
+  BL printf
+
+  # Return to OS
+  LDR lr, [sp, #0]
+  ADD sp, sp, #4
+  MOV pc, lr
+
+.data
+
+  #question for the user
+  question; .asciz "Please enter your age: "
+
+  #Format to read as an integer
+  input: .asciz "%d"
+
+  #Format of output
+  #Should have a tab before and after each character
+  format: .asciz "Your age is \"\t %d \t\". \n"
+
+  #Reserves space in mem for age
+  age: .word 0
