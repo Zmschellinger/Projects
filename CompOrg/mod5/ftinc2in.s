1e467845d153a5df7dd904d84c7e5c56f3d5a305
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-28 10:30:56 -0400

    Update ftinc2in.s

--- CompOrg/mod5/ftinc2in.s
+++ CompOrg/mod5/ftinc2in.s
@@ -12,18 +12,28 @@ main:
   SUB sp, sp, #4
   STR lr, [sp, #0]
 
-  #Ask user for their age
-  LDR r0, =question
+  #Ask user for ft and scan/store
+  LDR r0, =prompt1
   BL printf
-
-  # scanf
-  LDR r0, =input
-  LDR r1, =age
+  LDR r0, =input1
+  LDR r1, =feet
   BL scanf
 
+  #Ask user for inch and scan/store
+  LDR r0, =prompt2
+  BL printf
+  LDR r0, =input2
+  LDR r1, =inch
+  BL scanf
+
+  # convert feet to inches
+
+  # add converted feet to stored inches 
+
+  
   # print message
   LDR r0, =format
-  LDR r1, =age
+  LDR r1, =inch
   LDR r1, [r1, #0]
   BL printf
 
@@ -35,14 +45,17 @@ main:
 .data
 
   #question for the user
-  question; .asciz "Please enter your age: "
-
+  prompt1: .asciz "Please enter the number of feet: "
+  prompt2: .asciz "Please enter the number of inches: "
+  
   #Format to read as an integer
-  input: .asciz "%d"
-
+  input1: .asciz "%d"
+  input2: .asciz "%d"
+  
   #Format of output
   #Should have a tab before and after each character
-  format: .asciz "Your age is \"\t %d \t\". \n"
+  format: .asciz "The total amount of inches is %d. \n"
 
-  #Reserves space in mem for age
-  age: .word 0
+  #Reserves space in mem for feet and inch 
+  feet : .word 0
+  inch : .word 0
