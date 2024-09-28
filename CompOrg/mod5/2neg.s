3a834af5b2f730fb0029677abea3b3c0b4ef2382
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-28 12:32:07 -0400

    Update 2neg.s

--- CompOrg/mod5/2neg.s
+++ CompOrg/mod5/2neg.s
@@ -3,3 +3,50 @@
 # EN.605.204.81.FA24
 # Homework for module 5
 # This program will take an integer number and write out the negative value of the number. 
+.text
+.global main
+
+main: 
+  #Save return to os on stack (from textbook)
+  SUB sp, sp, #4
+  STR lr, [sp, #0]
+
+  #Ask user for the number they want to be negative
+  LDR r0, =prompt
+  BL printf
+
+  # scanf
+  LDR r0, =input
+  LDR r1, =num
+  BL scanf
+
+  #convert to negative
+  
+
+
+  
+  # print message
+  LDR r0, =format
+  LDR r1, =num
+  LDR r1, [r1, #0]
+  BL printf
+
+  # Return to OS
+  LDR lr, [sp, #0]
+  ADD sp, sp, #4
+  MOV pc, lr
+
+.data
+
+  #question for the user
+  prompt: .asciz "Please enter the number you want to negate: "
+
+  #Format to read as an integer
+  input: .asciz "%d"
+
+  #Format of output
+  #Should have a tab before and after each character
+  format: .asciz "The output is %d \n"
+
+  #Reserves space in mem for age
+  num: .word 0
