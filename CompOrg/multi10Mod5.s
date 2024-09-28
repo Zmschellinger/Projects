e36a1ac2f1eaf14564e0cc9225f2767f0f8fcc8c
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-28 09:30:39 -0400

    Update multi10Mod5.s

--- CompOrg/multi10Mod5.s
+++ CompOrg/multi10Mod5.s
@@ -39,18 +39,21 @@ main:
 
   #question for the user
   question; .asciz "Please enter a number: "
-
   #Format to read as an integer
   input: .asciz "%d"
-
   #Format of output
   #Should have a tab before and after each character
   format: .asciz "Your number multiplied by 10 is %d"
-
   #Reserves space in mem for age
   num: .word 0
 
 .txt
-#multi function
-mulit:
+# multi function
+# Left logical shift 3 times, then add the result of 1 left logical shift. 
+# num stored in r0
+multi:
+  MOV r3, r0, LSL #3
+  MOV r4, r0, LSL #1
+  ADD r0, r3, r4
+  
   
