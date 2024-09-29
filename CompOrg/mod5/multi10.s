4de0b178dc8afeb2ec270a6a801ae6d251d7418e
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-09-29 19:11:07 -0400

    Update multi10.s

--- CompOrg/mod5/multi10.s
+++ CompOrg/mod5/multi10.s
@@ -23,14 +23,14 @@ main:
   # Left logical shift 3 times, then add the result of 1 left logical shift. 
   # num stored in r0
   LDR r0, =num
+  LDR r0, [r0, #0]
   MOV r3, r0, LSL #3
   MOV r4, r0, LSL #1
   ADD r0, r3, r4
   
   # print message
+  MOV r1, r0
   LDR r0, =format
-  LDR r1, =num
-  LDR r1, [r1, #0]
   BL printf
 
   # Return to OS
