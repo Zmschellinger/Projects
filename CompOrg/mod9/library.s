4ac234177e48f7798981aac08914e3192c7c90fd
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-10-31 14:54:20 -0400

    Update library.s

--- CompOrg/mod9/library.s
+++ CompOrg/mod9/library.s
@@ -16,30 +16,40 @@
 
 # User input is saved in r9
 # Output will be saved in r1
+# logically determine if user input is an alphabetic character
+# What does logically mean? Using logical operations such as AND, NOT, OR ETC
+# This function will also print wither the input is an alphabetic character
+
 Logical_Check:
 
   	# Save return to os on stack (from textbook)
   	SUB sp, sp, #4
   	STR lr, [sp, #0]
 
-	# Move user input to r1
-	MOV r1, r9
-	# Compare input to Z, if higher check to see if lowercase
-	CMP r1, #90
-	BHI Logical_lowercase
-
-	# Compare input to A, if lower send to invalid
-	CMP r1, #65
-	BCC Invalid
-	B Valid
-
-Logical_lowercase:
-	CMP r1, #122
-	BHI Invalid
-	CMP r1, #97
-	BCC Invalid
-	B Valid
-
+	# if ((r9>=0x41 && r9=<0x5a) || (r9>=0x61 && r9<= 0x7a))
+	# User input is stored in r9
+	# From class readings to determine if an input is a character 
+	MOV r2, #0
+	CMP r9, #0x41
+	ADDGE r2, #1
+	MOV r3, #0
+	CMP r9, #0x5A
+	ADDLE r3, #1
+	AND r2, r2, r3
+	MOV r0, #0
+	CMP r9, #0x61
+	ADDGE r3, #1
+	MOV r3, #0
+	CMP r9, #0x7A
+	ADDLE r0, #1
+	AND r3, r3, r0
+	ORR r2, r2, r3 
+	
+	# if r2 is 1, then the user input is a letter
+	CMP r2, #1
+	B Valid 
+	B Invalid
+	
 # -------------------------------------------------------------------
 
 # User input is saved in r9
@@ -49,25 +59,34 @@ Non_Logical_Check:
 	# Save return to os on stack (from textbook)
   	SUB sp, sp, #4
   	STR lr, [sp, #0]
-	# Move user input to r1
-	MOV r1, r9
-	# subtract Z from user input, if negative, check to see if lowercase
-	SUB r2, r1, #90
-	CMP r2, #0
-	# If greater than 0, the number is invalid 
-	BPL Non_Logical_Lowercase
-  	CMP r1, #65
-   	BCC Invalid
-    	B Valid
 
-Non_Logical_Lowercase:
-	# subtract 122 or 'z' from the user input value
-	SUB r2, r1, #122
+	# if ((r9>=0x41 && r9=<0x5a) || (r9>=0x61 && r9<= 0x7a))
+	# subtract A from user input, if negative, check to see if lowercase
+	SUB r2, r9, #0x41
 	CMP r2, #0
-	BPL Invalid
-	CMP r1, #97
-	BCC Invalid
-	B Valid
+	
+	# If less than 0, the number is invalid 
+	BLT Invalid
+	
+	# subtract Z from user input, if less then 0, number is capital
+	# not less then 0, it might be lowercase
+	SUB r2, r9, #0x5A
+  	CMP r2, #0
+	BLT Valid
+
+	# subtract 'a' from the user input value
+	# if less then 0, user input is invalid
+	SUB r2, r1, #0x61
+	CMP r2, #0
+	BLT Invalid
+	
+	# subtract 'z' from user input value
+	# if less then 0, user input is valid
+	# if not, user input is invalid
+	SUB r2, r1, #0x7A
+	CMP r1, #0
+	BLT Valid
+	B Invalid
 
 
 # -------------------------------------------------------------------
@@ -85,6 +104,7 @@ Valid:
 # -------------------------------------------------------------------
 
 Invalid:
+	
 	LDR r0, =f1format2
 	BL printf
 
@@ -100,29 +120,30 @@ Invalid:
 # outputs largest number in r1
 # Finds largest of 3 numbers 
 findMax:
+
 	# Compare it to num 2
 	CMP r4, r5
 	BLE Else
-		# num1 larger then num2
-		# Compare it to num 3
-		CMP r4, r6
-		BLE num1_Greater
-		# If num 3 is larger, move to r0 and return 
-		MOV r1, r6
-		B EndIf
-		# if num 1 is greater
-		num1_Greater:
-		MOV r1, r4
-		B EndIf
+	# num1 larger then num2
+	# Compare it to num 3
+	CMP r4, r6
+	BLE num1_Greater
+	# If num 3 is larger, move to r0 and return 
+	MOV r1, r6
+	B EndIf
+	# if num 1 is greater
+	num1_Greater:
+	MOV r1, r4
+	B EndIf
 	# num 2 is larger
 	Else:	
-		# compare num 2 and num3
-		CMP r5, r6
-		BLE num2_greater
-		MOV r1, r6
-		B EndIf	
-		num2_greater:
-		MOV r1, r5
+	# compare num 2 and num3
+	CMP r5, r6
+	BLE num2_greater
+	MOV r1, r6
+	B EndIf	
+	num2_greater:
+	MOV r1, r5
 
 	EndIf:
 
