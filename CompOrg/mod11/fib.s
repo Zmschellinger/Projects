865bc5072448d4be9c0755e6df8f4cf0d1d43907
Zmschellinger <83358508+Zmschellinger@users.noreply.github.com>
2024-11-10 18:08:03 -0500

    Update fib.s

--- CompOrg/mod11/fib.s
+++ CompOrg/mod11/fib.s
@@ -5,7 +5,7 @@
 # This program will:
 # calculate a Fibonacci number recursively using a user input
 
-	
+
 .text
 .global main
 
@@ -15,76 +15,76 @@ main:
         SUB sp, sp, #4
         STR lr, [sp, #0]
 
-	# -----------------------------------------------
+        # -----------------------------------------------
 
-	# print prompt
-	LDR r0, =prompt
-	BL printf
+        # print prompt
+        LDR r0, =prompt
+        BL printf
 
-	# Read user input
-	LDR r0, =input
-	LDR r1, =in1
-	BL scanf
+        # Read user input
+        LDR r0, =input
+        LDR r1, =in1
+        BL scanf
 
-	# save input
-	LDR r1, =in1
-	LDR r9, [r1, #0]
-	
-	# user input is 0
-	MOV r7, #0
-	CMP r9, r7
-	BEQ print
-	
-	# initiate variables
-	MOV r8, #1
-	MOV r7, #1
-	MOV r6, #1
-	MOV r5, #0
-	
-	# user input is 1
-	CMP r9, r7
-	BEQ print
+        # save input
+        LDR r1, =in1
+        LDR r9, [r1, #0]
+
+        # user input is 0
+        MOV r7, #0
+        CMP r9, r7
+        BEQ print
+
+        # initiate variables
+        MOV r8, #1
+        MOV r7, #1
+        MOV r6, #1
+        MOV r5, #0
+
+        # user input is 1
+        CMP r9, r7
+        BEQ print
 
 # r9 - user input
 # r8 - loop counter
 # r7 - fib number
 # r6 - prev fib number
-# r5 - prev prev fib number	
+# r5 - prev prev fib number
 
 fib:
-	CMP r8, r9
-	BEQ print
-	
-	# loop counter not equal
-	ADD r7, r6, r5
-	
-	# INC counter
-	ADD r8, #1
+        CMP r8, r9
+        BEQ print
 
-	# update variables
-	MOV r5, r6
-	MOV r6, r7
+        # loop counter not equal
+        ADD r7, r6, r5
 
-	B fib
+        # INC counter
+        ADD r8, #1
+
+        # update variables
+        MOV r5, r6
+        MOV r6, r7
+
+        B fib
 
 print:
-	LDR r0, =output
-	MOV r1, r7
-	BL printf
+        LDR r0, =output
+        MOV r1, r7
+        BL printf
 
-end: 
-	LDR lr, [sp, #0]
+end:
+        LDR lr, [sp, #0]
         ADD sp, sp, #4
         MOV pc, lr
-	
-	
-	# -----------------------------------------------
-.data 
-	
-	prompt: .asciz "Please enter a number used to find its corresponding Fibonacci number: "
 
-	input: .asciz "%d"
 
-	output: .asciz "The corresponding Fibonacci number is %d.\n"
+        # -----------------------------------------------
+.data
 
-	in1: .word 0
+        prompt: .asciz "Please enter a number used to find its corresponding Fibonacci number: "
+
+        input: .asciz "%d"
+
+        output: .asciz "The corresponding Fibonacci number is %d.\n"
+
+        in1: .word 0
